d3f6e5a9e5308b1eca872924f5f859c75a55bbb2
Markus Unger <schaulustig23@gmail.com>
2021-11-21 13:35:19 +0100

    chore: eslint + prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,8 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'airbnb-base',
-        'plugin:import/typescript',
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-    plugins: ['@typescript-eslint', '@typescript-eslint/tslint', 'import', 'prefer-arrow', 'jest'],
+    plugins: ['@typescript-eslint', 'import', 'jest'],
 };
