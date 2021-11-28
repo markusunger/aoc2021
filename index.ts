467957bc4b830643f881a46699e4fc4b9b5d6230
Markus Unger <schaulustig23@gmail.com>
2021-11-28 14:47:52 +0100

    feat: handle missing or wrong startup argument

--- index.ts
+++ index.ts
@@ -8,7 +8,12 @@ import { getDayDirectoryString } from './utils';
 const [, , dayString] = process.argv;
 const day = parseInt(dayString, 10);
 
-console.log(process.argv[1]);
+if (Number.isNaN(day)) {
+    console.error(
+        'Please start with npm start <day> - day being the numerical value of the day you want to solve the puzzle for.',
+    );
+    process.exit(1);
+}
 
 async function main() {
     let solutionExists = false;
